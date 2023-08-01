992b12a7ed766ebd5755d04c9c624dc69e8b08fa
Manoj Kumar <manojsfstm5@gmail.com>
2023-08-01 20:14:37 +0530

    added one get route

--- controller/booking.controller.js
+++ controller/booking.controller.js
@@ -84,7 +84,7 @@ const gettingAllData=async(req,res)=>{
     const result=getBookings().find((ele)=>{
       return ele
     })
-    return res.status(200).json({message:result})
+    return res.status(200).json({result})
   } catch (error) {
     return res.status(500).json({ message: 'Unable to fetch data' });
   }
