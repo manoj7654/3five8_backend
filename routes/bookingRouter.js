7a4f8c0608eabba5bbc79c42bfe110170ec7b13c
Manoj Kumar <manojsfstm5@gmail.com>
2023-08-03 06:41:21 +0530

    updated controller and added booking details inside timeSlots array

--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -3,14 +3,13 @@ const express=require("express")
 const bookingRouter=express.Router()
 
 // importing bookFacility for making request
-const { bookingFacility, gettingAllData } = require("../controller/booking.controller")
+const { bookingFacility } = require("../controller/booking.controller")
 
 // making post request for newly bookings
 bookingRouter.post("/book",bookingFacility)
 
 
-// getting all booked facilities
-bookingRouter.get("/all",gettingAllData)
+
 
 
 module.exports={
